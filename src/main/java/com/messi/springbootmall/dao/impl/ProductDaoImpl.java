435f7f3ba3aafac843789dca77db992a2b70477c
PaulNMessi <exuberanteasy@gmail.com>
2026-05-20 11:43:43 +0800

    商品功能 - 實作新增商品

--- src/main/java/com/messi/springbootmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/messi/springbootmall/dao/impl/ProductDaoImpl.java
@@ -44,8 +44,8 @@ public class ProductDaoImpl implements ProductDao {
     public Integer createProduct(ProductRequest productRequest) {
         String sql = "INSERT INTO product (product_name, category, image_url, price, stock, " +
                 "description, created_date, last_modified_date) " +
-                "VALUES (:product_name, :category, :image_url, :price, :stock, :description, " +
-                ":created_date, :last_modified_date);";
+                "VALUES (:productName, :category, :imageUrl, :price, :stock, :description, " +
+                ":createdDate, :lastModifiedDate);";
 
         Map<String,Object> map = new HashMap<>();
         map.put("productName", productRequest.getProductName());
