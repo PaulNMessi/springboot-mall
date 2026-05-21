773f21f0f43a3fc825fb5f4d5c62e53ba33441be
PaulNMessi <exuberanteasy@gmail.com>
2026-05-21 10:58:09 +0800

    商品功能 - 實作查詢商品列表 (查詢條件)

--- src/main/java/com/messi/springbootmall/controller/ProductController.java
+++ src/main/java/com/messi/springbootmall/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.messi.springbootmall.controller;
 
+import com.messi.springbootmall.constant.ProductCategory;
 import com.messi.springbootmall.dto.ProductRequest;
 import com.messi.springbootmall.model.Product;
 import com.messi.springbootmall.service.ProductService;
@@ -20,8 +21,11 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/products")
-    public ResponseEntity<List<Product>> getProducts() {
-        List<Product> productList = productService.getProducts();
+    public ResponseEntity<List<Product>> getProducts(
+            @RequestParam(required = false) ProductCategory category,
+            @RequestParam(required = false) String search
+    ) {
+        List<Product> productList = productService.getProducts(category, search);
 
         return ResponseEntity.status(HttpStatus.OK).body(productList);
     }
