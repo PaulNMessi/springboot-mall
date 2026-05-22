9f3855d32a4886f50953278fcc1ad7c72f8b4df5
PaulNMessi <exuberanteasy@gmail.com>
2026-05-22 11:19:51 +0800

    商品功能 - 實作查詢商品列表 (分頁 + 總筆數)

--- src/main/java/com/messi/springbootmall/controller/ProductController.java
+++ src/main/java/com/messi/springbootmall/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.messi.springbootmall.dto.ProductQueryParams;
 import com.messi.springbootmall.dto.ProductRequest;
 import com.messi.springbootmall.model.Product;
 import com.messi.springbootmall.service.ProductService;
+import com.messi.springbootmall.util.Page;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
@@ -26,7 +27,7 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/products")
-    public ResponseEntity<List<Product>> getProducts(
+    public ResponseEntity<Page<Product>> getProducts(
 
             // 查詢條件 Filtering
             @RequestParam(required = false) ProductCategory category,
@@ -49,9 +50,20 @@ public class ProductController {
         productQueryParams.setLimit(limit);
         productQueryParams.setOffset(offset);
 
+        // 取得 product list
         List<Product> productList = productService.getProducts(productQueryParams);
 
-        return ResponseEntity.status(HttpStatus.OK).body(productList);
+        // 取得 product 總數
+        Integer total = productService.countProduct(productQueryParams);
+
+        // 分頁
+        Page<Product> page = new Page<>();
+        page.setLimit(limit);
+        page.setOffset(offset);
+        page.setTotal(total);
+        page.setResults(productList);
+
+        return ResponseEntity.status(HttpStatus.OK).body(page);
     }
 
     @GetMapping("/products/{productId}")
